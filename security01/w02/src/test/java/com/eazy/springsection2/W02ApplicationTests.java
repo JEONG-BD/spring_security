7dbc5128d03c8469317dbc6ff2570e618bf12d4a
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-05 15:27:03 +0900

    controller

---
+++ security01/w02/src/test/java/com/eazy/springsection2/W02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazy.springsection2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
