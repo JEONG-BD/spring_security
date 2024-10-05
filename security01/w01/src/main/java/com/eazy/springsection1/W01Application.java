81e659c85d77932647ad5a80a4507502963973e1
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-05 12:56:21 +0900

    security start

---
+++ security01/w01/src/main/java/com/eazy/springsection1/W01Application.java
@@ -0,0 +1,13 @@
+package com.eazy.springsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W01Application.class, args);
+    }
+
+}
