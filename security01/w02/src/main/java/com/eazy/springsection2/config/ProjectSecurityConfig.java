e7928cc383e251e58f80354c56becaf975da6b8f
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-07 09:04:32 +0900

    CompromisePasswordChecker

--- security01/w02/src/main/java/com/eazy/springsection2/config/ProjectSecurityConfig.java
+++ security01/w02/src/main/java/com/eazy/springsection2/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package com.eazy.springsection2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -37,11 +39,18 @@ public class ProjectSecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(){
-       UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
+       UserDetails user = User.withUsername("user").password("{noop}User@12345").authorities("read").build();
        //UserDetails admin = User.withUsername("admin").password("{noop}12345").authorities("admin").build();
-       UserDetails admin = User.withUsername("admin").password("$2a$12$ugozWIb018u6vUVGanxUkO6QyQoxhf4E8HBKl.nqcMaq1AXTyRN4q").authorities("admin").build();
+       //eazy@54321
+       UserDetails admin = User.withUsername("admin").password("$2a$12$9RbTyFkznZaMk5sU3MDuJe5vewdUd.jI/hYq5GUY80NBzgcvHWeNW").authorities("admin").build();
        return new InMemoryUserDetailsManager(user, admin);
     }
+
+//
+//    @Bean
+//    public CompromisedPasswordChecker compromisedPasswordChecker(){
+//        return new HaveIBeenPwnedRestApiPasswordChecker();
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
