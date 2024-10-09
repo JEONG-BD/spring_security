2f5c5c47420c32814b4a60c195bed62d8c600648
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-09 13:39:16 +0900

    csrf, register

--- security01/w03/src/main/java/com/eazy/springsection3/config/ProjectSecurityConfig.java
+++ security01/w03/src/main/java/com/eazy/springsection3/config/ProjectSecurityConfig.java
@@ -21,9 +21,10 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests((request)-> request
-                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "myCards").authenticated()
-                .requestMatchers("/notices", "/contact", "/error", "/welcome").permitAll());
+        http.csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests((request)-> request
+                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "myCards").authenticated()
+                        .requestMatchers("/notices", "/contact", "/error", "/welcome", "/register").permitAll());
 
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
