ed7d61b29fc194d2a06b3030893c246df2a4c4d1
JEONG-BD <dev.byeongdu@gmail.com>
2024-10-09 12:57:05 +0900

    model, repository

---
+++ security01/w03/src/test/java/com/eazy/springsection3/W03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazy.springsection3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
