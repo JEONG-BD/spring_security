4f6ab27cfd0b9f34b68a856dc51a33afc285f47c
JEONG-BD <dev.byeongdu@gmail.com>
2024-11-14 08:42:04 +0900

    restart security

---
+++ security01/w04/src/main/java/com/example/w04/W04Application.java
@@ -0,0 +1,13 @@
+package com.example.w04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W04Application.class, args);
+    }
+
+}
---
+++ security01/w04/src/test/java/com/example/w04/W04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
