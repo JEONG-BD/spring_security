aacbcb644a008b58153e9a8a7d726ec88a822cd9
JEONG-BD <dev.byeongdu@gmail.com>
2024-11-19 08:37:41 +0900

    properties

--- security01/w04/src/main/java/com/example/w04/W04Application.java
+++ security01/w04/src/main/java/com/example/w04/W04Application.java
@@ -2,7 +2,9 @@ package com.example.w04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+//@ComponentScan("com.example.w04.controller")
 @SpringBootApplication
 public class W04Application {
 
