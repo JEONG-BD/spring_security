d8146764f5db8d3bf13aea461a7b6a447288c82d
JEONG-BD <dev.byeongdu@gmail.com>
2024-12-10 08:26:07 +0900

    UserDetailsService

--- security01/w04/src/main/java/com/example/w04/config/ProjectSecurityConfig.java
+++ security01/w04/src/main/java/com/example/w04/config/ProjectSecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -22,11 +26,25 @@ public class ProjectSecurityConfig {
                 requests
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                         .requestMatchers("/notices", "/contact", "/error").permitAll());
-        // http.formLogin(withDefaults());
+         http.formLogin(withDefaults());
+         http.httpBasic(withDefaults());
+        /*
         http.formLogin((httpSecurityFormLoginConfigurer) ->
                 httpSecurityFormLoginConfigurer.disable());
         http.httpBasic(HttpBasicConfigurer ->
                 HttpBasicConfigurer.disable());
+        */
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("root").password("12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("12345").authorities("admin").build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+
+
 }
