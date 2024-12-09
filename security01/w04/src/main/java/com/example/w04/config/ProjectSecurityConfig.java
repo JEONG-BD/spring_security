4f0b6742145405d7e00f9fb9a050c8395a443607
JEONG-BD <dev.byeongdu@gmail.com>
2024-12-09 12:43:45 +0900

    formLogin, httpBasic

--- security01/w04/src/main/java/com/example/w04/config/ProjectSecurityConfig.java
+++ security01/w04/src/main/java/com/example/w04/config/ProjectSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -21,10 +22,11 @@ public class ProjectSecurityConfig {
                 requests
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                         .requestMatchers("/notices", "/contact", "/error").permitAll());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        // http.formLogin(withDefaults());
+        http.formLogin((httpSecurityFormLoginConfigurer) ->
+                httpSecurityFormLoginConfigurer.disable());
+        http.httpBasic(HttpBasicConfigurer ->
+                HttpBasicConfigurer.disable());
         return http.build();
     }
-
-
 }
