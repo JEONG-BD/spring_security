f0aa76ad0f5d747b124d4ea7ab4e17218d6932cc
JEONG-BD <dev.byeongdu@gmail.com>
2024-12-30 08:59:43 +0900

    CustomUserDetailService

--- security01/w04/src/main/java/com/example/w04/W04Application.java
+++ security01/w04/src/main/java/com/example/w04/W04Application.java
@@ -2,10 +2,16 @@ package com.example.w04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 //@ComponentScan("com.example.w04.controller")
 @SpringBootApplication
+@EnableJpaRepositories("com.example.w04.repository")
+@EnableWebSecurity
+@EntityScan("com.example.w04")
 public class W04Application {
 
     public static void main(String[] args) {
---
+++ security01/w04/src/main/java/com/example/w04/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.example.w04.repository;
+
+import com.example.w04.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
+
+}
