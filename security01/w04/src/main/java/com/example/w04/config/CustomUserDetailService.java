0003a86835aba1130a9c76847bd7eeae08562f6f
JEONG-BD <dev.byeongdu@gmail.com>
2025-01-02 08:07:43 +0900

    loadByUsername

--- security01/w04/src/main/java/com/example/w04/config/CustomUserDetailService.java
+++ security01/w04/src/main/java/com/example/w04/config/CustomUserDetailService.java
@@ -1,14 +1,30 @@
 package com.example.w04.config;
 
+import com.example.w04.model.Customer;
+import com.example.w04.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
+
+
+    private final CustomerRepository customerRepository;
+
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Customer customer = customerRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User detail not found for the user " + username));
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        return new User(customer.getEmail(),customer.getPwd(),authorities);
     }
 }
--- security01/w04/src/main/java/com/example/w04/config/ProjectSecurityConfig.java
+++ security01/w04/src/main/java/com/example/w04/config/ProjectSecurityConfig.java
@@ -52,11 +52,11 @@ public class ProjectSecurityConfig {
 //        return new InMemoryUserDetailsManager(user, admin);
 //    }
 
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
-
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//
+//    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
