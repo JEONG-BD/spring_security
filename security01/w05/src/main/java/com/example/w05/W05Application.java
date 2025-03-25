76a9f486fe07938fd1ef2bd926e290b72c3afe83
JEONG-BD <dev.byeongdu@gmail.com>
2025-03-26 08:44:19 +0900

    refresh

---
+++ security01/w05/src/main/java/com/example/w05/W05Application.java
@@ -0,0 +1,13 @@
+package com.example.w05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W05Application.class, args);
+    }
+
+}
---
+++ security01/w05/src/test/java/com/example/w05/W05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
